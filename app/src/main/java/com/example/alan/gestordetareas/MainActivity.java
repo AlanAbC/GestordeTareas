29494d45a49cfa68b17b1a86a1ed5c0ba6853225
AlanAbC <alanjiraya77@gmail.com>
2017-03-30 19:58:28 -0600

    Se modifico

--- app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
+++ app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.alan.gestordetareas;
 
+import android.graphics.Point;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Window;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    int width;
+    int height;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
     }
 }
