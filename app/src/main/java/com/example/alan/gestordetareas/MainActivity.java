4f5d9e3ecf1891e011f943822db963ddb82bcabd
Elias Rosales <mansion1@hotmail.com>
2017-03-31 11:46:29 -0600

    hola

--- app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
+++ app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-
+        hola mundo;
 
     }
 }
