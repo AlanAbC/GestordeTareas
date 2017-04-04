5b90d361bb0bed36760dd47e46181df95ee71e37
AlanAbC <alanjiraya77@gmail.com>
2017-04-04 08:35:24 -0500

    se modifico agregarTarea

--- app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
+++ app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.alan.gestordetareas;
 
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     int width;
+    FloatingActionButton agregar;
     int height;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        agregar=(FloatingActionButton)findViewById(R.id.agregar);
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,agregarTarea.class);
+                startActivity(i);
+            }
+        });
     }
 }
