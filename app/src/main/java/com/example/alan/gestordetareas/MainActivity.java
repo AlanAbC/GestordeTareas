3240c33e9c1a3ee6314239e96643cd997f5fc8cc
Ricardo Antonio Pizano Pérez <rikymon2@hotmail.com>
2017-04-04 20:37:38 -0500

    Se creo el menu desplegable

--- app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
+++ app/src/main/java/com/example/alan/gestordetareas/MainActivity.java
@@ -3,25 +3,38 @@ package com.example.alan.gestordetareas;
 import android.content.Intent;
 import android.graphics.Point;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    int width;
-    int height;
-    FloatingActionButton agregar;
+    private FloatingActionButton agregar;
+    private DrawerLayout drawerLayout;
+    final List<MenuItem> items = new ArrayList<>();
+    private Menu menu;
+    NavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.dLayout);
         agregar=(FloatingActionButton)findViewById(R.id.agregar);
         agregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,10 +43,43 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        nav = (NavigationView)findViewById(R.id.navigation);
+        menu = nav.getMenu();
+        for(int i = 0; i < menu.size(); i++){
+            items.add(menu.getItem(i));
+        }
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                int pos = items.indexOf(item);
+                if(pos == 0){
+                    msg("Materias");
+                }else if(pos == 1){
+                    msg("Tareas Completadas");
+                }else if(pos == 2){
+                    Intent i = new Intent(MainActivity.this, agregarTarea.class);
+                    startActivity(i);
+                }else if(pos == 3){
+                    msg("Horario");
+                }else if(pos == 4){
+                    msg("Acerca de");
+                }
+                return false;
+            }
+        });
     }
 
     public void agregarTarea(View view){
         Intent i = new Intent(MainActivity.this, agregarTarea.class);
         startActivity(i);
+    }
+
+    public void abrirMenu(View view){
+
+    }
+
+    public void msg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
